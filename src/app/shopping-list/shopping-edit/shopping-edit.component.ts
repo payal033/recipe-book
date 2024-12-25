584c27408077bfe07ae72d273ad3d5122f5fbcab
Payal <payalkarwa99@gmail.com>
2024-12-25 18:29:35 +0530

    create all components

---
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-shopping-edit',
+  imports: [],
+  templateUrl: './shopping-edit.component.html',
+  styleUrl: './shopping-edit.component.css'
+})
+export class ShoppingEditComponent {
+
+}
