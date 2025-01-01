bc0f1585994c4f189267294d49984e7a07b7177a
Payal <payalkarwa99@gmail.com>
2025-01-02 00:47:35 +0530

    update Readme

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
 import { RouterOutlet } from '@angular/router';
-import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
